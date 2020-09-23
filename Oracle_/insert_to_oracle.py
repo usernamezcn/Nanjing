2cf8ce0c1a505019fa400fe8672d5e37a491f729
子辛 <48110583+usernamezcn@users.noreply.github.com>
2020-09-23 10:40:22 +0800

    数据库完善
    broken_result，没有保存到数据库中,So add

--- Oracle_/insert_to_oracle.py
+++ Oracle_/insert_to_oracle.py
@@ -9,16 +9,20 @@ import json
 import time
 os.environ['NLS_LANG'] = 'SIMPLIFIED CHINESE_CHINA.UTF8'
 
+
+
+
 class Ins2Orc(object):
     def __init__(self,user,pwd,ip,port,sid):
         self.connect=cx_Oracle.connect(user+"/"+pwd+"@"+ip+":"+port+"/"+sid)
         self.cursor=self.connect.cursor()
 
-    def insert_(state,b_r,result_dict,DEVICECODE,result_cp,end_result_cp):
-        test_oracle = TestOracle('mw_app', 'app', '127.0.0.1', '1521', 'DBORCALE')
+    def insert_(state,b_r,result_dict,DEVICECODE,result_cp,end_result_cp,broken_reason):
+        # test_oracle = TestOracle('mw_app', 'app', '127.0.0.1', '1521', 'DBORCALE')
+        test_oracle = TestOracle('mw_app', 'app', '192.168.2.200', '1521', 'ORCL')
         # param=[('30M00000059982619','(正常：0.3；异常：0.5；故障：0.2)','异常',time.strftime("%Y/%m/%D %H:%M:%S")),]
         param = [
-            (DEVICECODE, datetime.datetime.now(), state, b_r, result_dict['0'], result_dict['1'], result_dict['2']), ]
+            (DEVICECODE, datetime.datetime.now(), state, b_r, result_dict['0'], result_dict['1'], result_dict['2'],broken_reason), ]
         param_1 = [
             (DEVICECODE, datetime.datetime.now(), int(result_cp.iloc[0, 0]), int(result_cp.iloc[0, 1]),
              int(result_cp.iloc[0, 2]),
@@ -36,7 +40,7 @@ class Ins2Orc(object):
              SS['S1'], SS['S2'], SS['S3'], SS['S4'], SS['S5'], SS['S6'], SS['S7'], SS['S8'],
              SS['S9'], SS['S10'], SS['S11'], SS['S12'], SS['S13'], SS['S14'], SS['S15'],
              SS['S16'], SS['S17']), ]
-        sql_insert = 'insert into BHT_DEVICE_STATUS(DEVICECODE,TIME,DEVICE_STATUS,BROKEN_RESULT,STATUS_0,STATUS_1,STATUS_2)values(:1,:2,:3,:4,:5,:6,:7)'
+        sql_insert = 'insert into BHT_DEVICE_STATUS(DEVICECODE,TIME,DEVICE_STATUS,BROKEN_RESULT,STATUS_0,STATUS_1,STATUS_2,BROKEN_REASON)values(:1,:2,:3,:4,:5,:6,:7,:8)'
         sql_insert_1 = 'insert into BHT_DEVICE_MIDDLE_PARTONE(DEVICECODE,TIME,DEVICE_OBNORMAL_X1,DEVICE_OBNORMAL_X2,DEVICE_OBNORMAL_X3,DEVICE_OBNORMAL_X4,DEVICE_OBNORMAL_X5,DEVICE_OBNORMAL_X6,DEVICE_OBNORMAL_X7,DEVICE_OBNORMAL_X8,DEVICE_OBNORMAL_X9)values(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11)'
         sql_insert_2 = 'insert into BHT_DEVICE_MIDDLE_PARTTWO(DEVICECODE,TIME,DEVICE_FAULT_S1,DEVICE_FAULT_S2,DEVICE_FAULT_S3,DEVICE_FAULT_S4,DEVICE_FAULT_S5,DEVICE_FAULT_S6,DEVICE_FAULT_S7,DEVICE_FAULT_S8,DEVICE_FAULT_S9,\
                                     DEVICE_FAULT_S10,DEVICE_FAULT_S11,DEVICE_FAULT_S12,DEVICE_FAULT_S13,DEVICE_FAULT_S14,DEVICE_FAULT_S15,DEVICE_FAULT_S16,DEVICE_FAULT_S17,\
@@ -54,16 +58,17 @@ class Ins2Orc(object):
             test_oracle.insert(sql_insert, param)
         except:
             print('最终表插入失败')
-        test_oracle = TestOracle('mw_app', 'app', '127.0.0.1', '1521', 'DBORCALE')
+        test_oracle = TestOracle('mw_app', 'app', '192.168.2.200', '1521', 'ORCL')
         try:
             test_oracle.insert(sql_insert_1, param_1)
             print('中间表一插入成功')
         except:
             print('中间表一插入失败')
-        test_oracle = TestOracle('mw_app', 'app', '127.0.0.1', '1521', 'DBORCALE')
+        test_oracle = TestOracle('mw_app', 'app', '192.168.2.200', '1521', 'ORCL')
         try:
             test_oracle.insert(sql_insert_2, param_2)
             print('中间表二插入成功')
         except:
             print('中间表二插入失败')
+
 
