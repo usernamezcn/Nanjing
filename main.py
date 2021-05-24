d30ae4740eab8e25ab41df9bf6077ffedea14c61
子辛 <48110583+usernamezcn@users.noreply.github.com>
2021-05-24 17:12:33 +0800

    增加了定时器的功能！
    更改数据库，给用户增加了选择的权利，
    operation_cycles 
    输电线路在线监测装置可靠性评估周期。有四种取值：0--立即执行、1--每月执行一次、2--每周执行一次、3--每天执行一次。

--- main.py
+++ main.py
@@ -6,33 +6,33 @@ from data_.model_one import main_model_one
 from bayes_.bayes_model import main_model_two
 from Oracle_.Traversing_database import load_data_from_orcale
 from fuzzy_.fuzzy_model import weight, W_weight, s_mat_weight
+from parameter_Definition import operation_cycles
 from Oracle_.oracle import TestOracle
-from fault_.fault_describe import univariate
+from fault_.fault_describe import univariate,univariate_dist
 from Oracle_.Insert_to_orcale import Ins2Orc
 from print_.to_pic import plt2pic
 import numpy as np
 import pandas as pd
-import datetime,time,os,io
+import datetime, time, os,io,re
 import schedule
 import configparser
 
 pd.set_option('display.max_columns', 1000)
-pd.set_option('display.width', 1000)       #输出全部元素
+pd.set_option('display.width', 1000)  # 输出全部元素
 
-init_end_result = [0.0]*17
+init_end_result = [0.0] * 17
 init_end_result = pd.DataFrame(init_end_result).T
-init_end_result.columns = ['S1_0.0','S2_0.0','S3_0.0','S4_0.0','S5_0.0','S6_0.0','S7_0.0','S8_0.0',
-                        'S9_0.0','S10_0.0','S11_0.0','S12_0.0','S13_0.0','S14_0.0','S15_0.0','S16_0.0',
-                        'S17_0.0']
-init_result = pd.DataFrame([0.0]*9,index = ['X1','X2','X3','X4','X5','X6','X7','X8','X9']).T
-init_result_dict = {'0':1.0,'1':0.0,'2':0.0}
+init_end_result.columns = ['S1_0.0', 'S2_0.0', 'S3_0.0', 'S4_0.0', 'S5_0.0', 'S6_0.0', 'S7_0.0', 'S8_0.0',
+                           'S9_0.0', 'S10_0.0', 'S11_0.0', 'S12_0.0', 'S13_0.0', 'S14_0.0', 'S15_0.0', 'S16_0.0',
+                           'S17_0.0']
+init_result = pd.DataFrame([0.0] * 9, index=['X1', 'X2', 'X3', 'X4', 'X5', 'X6', 'X7', 'X8', 'X9']).T
+init_result_dict = {'0': 1.0, '1': 0.0, '2': 0.0}
 
-def config():
-    cf = configparser.ConfigParser()
-    cf.read('F:/Reliability_Evaluation/config.ini')
-    return cf.get('time_setting','day'),cf.get('time_setting','clock')
+'''
+operation_cycles 
+输电线路在线监测装置可靠性评估周期。有四种取值：0--立即执行、1--每月执行一次、2--每周执行一次、3--每天执行一次。
+'''
 
-    pass
 
 class Logger(object):
     def __init__(self, filename="Default.log", path="./"):
@@ -47,181 +47,118 @@ class Logger(object):
     def flush(self):
         pass
 
+# 如果消除注释则会，运行批量数据，才会显示日志
+# sys.stdout = Logger('test_log.log')
+
+def config():
+    cf = configparser.ConfigParser()
+    cf.read('F:/Reliability_Evaluation/config.ini')
+    return cf.get('time_setting','day'),cf.get('time_setting','clock')
+    pass
 
 def main():
-    sys.stdout = Logger('main_log.log')
-    num_day,asdf = config()
-    # print(num_day)
-    # print(type(num_day))
-    # num_day = 1 #每月的一号开始运行，如果需要在每月2号运行，请将k改为2
-    # print(datetime.datetime.now().day,type(datetime.datetime.now().day))
-    if datetime.datetime.now().day == int(num_day):
-        try:
-            for i in load_data_from_orcale():
-                broken_reason = []
-                broken_result = []  # 用来存放异常/故障的原因，S1-S17
-                model_one_temp = 0
-                # print('输出判断值为：：：：：：：：：：：：：：：：：：：：：：：：',model_one_temp)
-                data_act = i['data']
+    for i in load_data_from_orcale():
+        broken_reason = []
+        broken_result = []  # 用来存放异常/故障的原因，S1-S17
+        model_one_temp = 0
+        data_act = i['data']
 
 
-                data_act = data_act.rolling(window=5, win_type='gaussian', center=True).mean(std=0.5)  # 平滑操作
+        data_act = data_act.rolling(window=5, win_type='gaussian', center=True).mean(std=0.5)  # 平滑操作
 
 
-                DEVICECODE = i['DeviceCode']
+        Monitoring_type_parameter = i['Monitoring_type_parameter']
+        # print(Monitoring_type_parameter)
+        DEVICECODE = i['DeviceCode']
+        result = main_model_one(data_act,Monitoring_type_parameter)
 
-                # print(i['MonitorTypeName'])
-                # if i['MonitorTypeName']=='风偏监测':
-                #     print(data_act)\
-                # time.sleep(1000)
-                # print('检测类型：{}                状态量为：{}'.format(i['MonitorTypeName'],univariate[i['MonitorTypeName']]))
-                # plt2pic(data_act)
-                result = main_model_one(data_act)
-                # print('模型给一出口')
-                # print('长度*********************************************：：：：：：：：：：：：：：：')
-                # print(len(data_act))
+        result_cp = result.__deepcopy__()
+        for i in range(9):
+            if result.iloc[0, i] == 1.0: model_one_temp = 1;break;
+        if model_one_temp == 0:  #如果没有任何装置异常，直接退出循环
+            Ins2Orc.insert_(state=0, b_r='正常', result_dict=init_result_dict, DEVICECODE=DEVICECODE,
+                            result_cp=result_cp, end_result_cp=init_end_result, broken_reason='无')
 
-                result_cp = result.__deepcopy__()
-                for i in range(9):
-                    if result.iloc[0,i] == 1.0:model_one_temp = 1;break;
+            continue
 
-                if model_one_temp == 0:
-                    Ins2Orc.insert_(state = 0,b_r='',result_dict=init_result_dict,DEVICECODE=DEVICECODE,result_cp=result_cp,end_result_cp=init_end_result,broken_reason='')
-                    # Ins2Orc.insert_(state=state, b_r=broken_result, result_dict=result_dict, DEVICECODE=DEVICECODE,
-                    #                 result_cp=result_cp,
-                    #                 end_result_cp=end_result_cp)
-                    continue
-                # plt2pic(data_act)
-                # time.sleep(4)
-                # print('result\n',result)
-                '''模型一的输出'''
-
-                # print('\n模型一输出数据中存在的数据异常模式:\n', result)
-                # print('模型一的输出结果----数据中存在的异常模式：')
-                for i in result.T.index:
-                    #print(result[i].values[0])
-                    if result[i].values[0] == 1.0:
-                        broken_result.append(X[i])
-                        # print(X[i])
-                # print('*************************模型一完美通过***********************')
-
-                '''模型二的输出'''
-                end_result = main_model_two(result)
-                '''
-                if result.values[0].tolist()==[0]*9:
-                    end_result = pd.DataFrame([0]*17,index=['S1_0.0', 'S2_0.0', 'S3_0.0', 'S4_0.0', 'S5_0.0', 'S6_0.0', 'S7_0.0', 'S8_0.0',
-                                               'S9_0.0', 'S10_0.0', 'S11_0.0', 'S12_0.0', 'S13_0.0', 'S14_0.0', 'S15_0.0',
-                                               'S16_0.0',
-                                               'S17_0.0']).T
-                '''
-
-                end_result_cp = end_result.__deepcopy__()
-                # print('\n模型二输出的故障类型及概率：\n', end_result)
-                # print('最大值：', max(end_result.values[0]))
-                max_value = max(end_result.values[0])
-
-                for i in end_result.columns:
-                    if float(max_value) == float(end_result[i]):
-                        broken_reason.append(S[i])
-                        #print('故障可能为{}:{}'.format(i,S[i]))
-                # print('*************************模型二完美通过***********************')
-                '''模型三的输出'''
-
-                result_dict = {}  # 用来存放模型三的结果
-                for i in range(len(end_result)):
-                    # print(S_Probability.loc[i].values)
-                    columns = ['S3_0.0', 'S7_0.0', 'S8_0.0', 'S9_0.0', 'S10_0.0', 'S11_0.0', 'S12_0.0',
-                               'S13_0.0', 'S14_0.0', 'S15_0.0', 'S16_0.0', 'S17_0.0']
-                    end_result = end_result[columns]
-                    w_weight = W_weight(s_mat_weight, end_result.loc[i].values)
-                    result = np.dot(w_weight, weight)
-                    result = result*100
-                    # print('\n模型三的评判结果：')
-                    '''0:正常 1：异常 2：故障'''
-                    result_ = pd.DataFrame(result, index=['正常', '异常', '故障'], columns=['result'])
-                    result_dict = {'0': round(result[0], 4), '1': round(result[1], 4), '2': round(result[2], 4)}
-                    # print(result_dict)
-                    state = max(result_dict, key=lambda x: result_dict[x])
-                    # print('设备状态：', state)
-                    # print('设备状态：', str(result'''输出检测时间：dd/mm/yyyy格式'''_[result_.result == float(max(result_.values))].index.tolist()[0]))
-                # print('检测时间：', datetime.datetime.now())
-                # time.sleep(100)
-                broken_result = ','.join(broken_result)  # 取出列表中的所有元素
-                broken_reason = ','.join(broken_reason)
-
-                # print('远程数据库测试-----------------------------------------------------:')
-                # print(broken_reason)
-
-                # print(broken_result)
-                # print('*************************模型三完美通过***********************')
-                '''模型三结果存入数据库'''
-                # test_oracle = TestOracle('mw_app', 'app', '192.168.2.200', '1521', 'ORCL')
-                Ins2Orc.insert_(state = state, b_r=broken_result, result_dict=result_dict, DEVICECODE=DEVICECODE, result_cp=result_cp,
-                                end_result_cp=end_result_cp,broken_reason = broken_reason)
-                # param=[('30M00000059982619','(正常：0.3；异常：0.5；故障：0.2)','异常',time.strftime("%Y/%m/%D %H:%M:%S")),]
-                '''
-                param = [(DEVICECODE, datetime.datetime.now(), state, broken_result, result_dict['0'], result_dict['1'], result_dict['2'],broken_reason), ]
-                param_1 = [
-                    (DEVICECODE, datetime.datetime.now(), int(result_cp.iloc[0, 0]), int(result_cp.iloc[0, 1]),
-                     int(result_cp.iloc[0, 2]),
-                     int(result_cp.iloc[0, 3]),
-                     int(result_cp.iloc[0, 4]), int(result_cp.iloc[0, 5]), int(result_cp.iloc[0, 6]), int(result_cp.iloc[0, 7]),
-                     int(result_cp.iloc[0, 8])), ]
-                param_2 = [(DEVICECODE, datetime.datetime.now(), float(end_result_cp.iloc[0, 0]), float(end_result_cp.iloc[0, 1]),
-                            float(end_result_cp.iloc[0, 2]), float(end_result_cp.iloc[0, 3]), float(end_result_cp.iloc[0, 4]) \
-                                , float(end_result_cp.iloc[0, 5]), float(end_result_cp.iloc[0, 6]), float(end_result_cp.iloc[0, 7]),
-                            float(end_result_cp.iloc[0, 8]), float(end_result_cp.iloc[0, 9]) \
-                                , float(end_result_cp.iloc[0, 10]), float(end_result_cp.iloc[0, 11]), float(end_result_cp.iloc[0, 12]),
-                            float(end_result_cp.iloc[0, 13]), float(end_result_cp.iloc[0, 14]) \
-                                , float(end_result_cp.iloc[0, 15]), float(end_result_cp.iloc[0, 16]),
-                            SS['S1'], SS['S2'], SS['S3'], SS['S4'], SS['S5'], SS['S6'], SS['S7'], SS['S8'],
-                            SS['S9'], SS['S10'], SS['S11'], SS['S12'], SS['S13'], SS['S14'], SS['S15'],
-                            SS['S16'], SS['S17']), ]
-                sql_insert = 'insert into BHT_DEVICE_STATUS(DEVICECODE,TIME,DEVICE_STATUS,BROKEN_REASULT,STATUS_0,STATUS_1,STATUS_2,BROKEN_REASON)values(:1,:2,:3,:4,:5,:6,:7,:8)'
-                sql_insert_1 = 'insert into BHT_DEVICE_MIDDLE_PARTONE(DEVICECODE,TIME,DEVICE_OBNORMAL_X1,DEVICE_OBNORMAL_X2,DEVICE_OBNORMAL_X3,DEVICE_OBNORMAL_X4,DEVICE_OBNORMAL_X5,DEVICE_OBNORMAL_X6,DEVICE_OBNORMAL_X7,DEVICE_OBNORMAL_X8,DEVICE_OBNORMAL_X9)values(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11)'
-                sql_insert_2 = 'insert into BHT_DEVICE_MIDDLE_PARTTWO(DEVICECODE,TIME,DEVICE_FAULT_S1,DEVICE_FAULT_S2,DEVICE_FAULT_S3,DEVICE_FAULT_S4,DEVICE_FAULT_S5,DEVICE_FAULT_S6,DEVICE_FAULT_S7,DEVICE_FAULT_S8,DEVICE_FAULT_S9,\
-                                        DEVICE_FAULT_S10,DEVICE_FAULT_S11,DEVICE_FAULT_S12,DEVICE_FAULT_S13,DEVICE_FAULT_S14,DEVICE_FAULT_S15,DEVICE_FAULT_S16,DEVICE_FAULT_S17,\
-                                        DEVICE_FAULT_S1_DATA_RESULT,DEVICE_FAULT_S2_DATA_RESULT,DEVICE_FAULT_S3_DATA_RESULT,DEVICE_FAULT_S4_DATA_RESULT,DEVICE_FAULT_S5_DATA_RESULT, \
-                                       DEVICE_FAULT_S6_DATA_RESULT,DEVICE_FAULT_S7_DATA_RESULT,DEVICE_FAULT_S8_DATA_RESULT,DEVICE_FAULT_S9_DATA_RESULT,DEVICE_FAULT_S10_DATA_RESULT,\
-                                       DEVICE_FAULT_S11_DATA_RESULT,DEVICE_FAULT_S12_DATA_RESULT,DEVICE_FAULT_S13_DATA_RESULT,DEVICE_FAULT_S14_DATA_RESULT,DEVICE_FAULT_S15_DATA_RESULT,\
-                                       DEVICE_FAULT_S16_DATA_RESULT,DEVICE_FAULT_S17_DATA_RESULT)values(:1,:2,:3,:4,:5,:6,:7,:8,:9,:10,:11,:12,:13,:14,:15,:16,:17,:18,:19,:20,:21\
-                                       ,:22,:23,:24,:25,:26,:27,:28,:29,:30,:31,:32,:33,:34,:35,:36)'
-            
-                # param = [(DEVICECODE, state, b_r, result_dict['0'], result_dict['1'], result_dict['2']), ]
-                try:
-                    # sql_insert = 'insert into BHT_DEVICE_STATUS(DEVICECODE,DEVICE_STATUS,BROKEN_RESULT,STATUS_0,STATUS_1,STATUS_2)values(:1,:3,:4,:5,:6,:7)'
-                    test_oracle.insert(sql_insert, param)
-            
-                except:
-                    print('最终表插入失败')
-            
-            
-            
-                test_oracle = TestOracle('mw_app', 'app', '127.0.0.1', '1521', 'DBORCALE')
-                try:
-                    test_oracle.insert(sql_insert_1, param_1)
-                except:
-                    print('中间表一插入失败')
-                test_oracle = TestOracle('mw_app', 'app', '127.0.0.1', '1521', 'DBORCALE')
-                try:
-                    test_oracle.insert(sql_insert_2, param_2)
-                except:
-                    print('中间表二插入失败')
-                '''
+        '''画图进行确认'''
+        plt2pic(data_act)
 
 
-                # print('一个数据跑完了&&&&&一个数据跑完了&&&&&一个数据跑完了&&&&&一个数据跑完了&&&&&一个数据跑完了&&&&&一个数据跑完了')
-                #time.sleep(4)
-        except:
-            pass
+        '''模型一的输出'''
 
+        print('模型一的输出结果----数据中存在的异常模式：',end=' ')
+        for i in result.T.index:
+            # print(result[i].values[0])  #输出的是 0 or 1
+            if result[i].values[0] == 1.0:
+                broken_result.append(X[i])
+                print(X[i],' ',i,end=' ')
+        print()
+
+
+
+        '''模型二的输出'''
+        end_result = main_model_two(result)
+        '''
+        if result.values[0].tolist()==[0]*9:
+            end_result = pd.DataFrame([0]*17,index=['S1_0.0', 'S2_0.0', 'S3_0.0', 'S4_0.0', 'S5_0.0', 'S6_0.0', 'S7_0.0', 'S8_0.0',
+                                       'S9_0.0', 'S10_0.0', 'S11_0.0', 'S12_0.0', 'S13_0.0', 'S14_0.0', 'S15_0.0',
+                                       'S16_0.0',
+                                       'S17_0.0']).T
+        '''
+
+        end_result_cp = end_result.__deepcopy__()
+        max_value = max(end_result.values[0])
+
+        for i in end_result.columns:
+            if float(max_value) == float(end_result[i]):
+                broken_reason.append(S[i])
+        result_dict = {}  # 用来存放模型三的结果
+
+        print('len(end_result)',len(end_result))
+        for i in range(len(end_result)):
+
+            columns = ['S3_0.0', 'S7_0.0', 'S8_0.0', 'S9_0.0', 'S10_0.0', 'S11_0.0', 'S12_0.0',
+                       'S13_0.0', 'S14_0.0', 'S15_0.0', 'S16_0.0', 'S17_0.0']
+            end_result = end_result[columns]
+            w_weight = W_weight(s_mat_weight, end_result.loc[i].values)
+            result = np.dot(w_weight, weight)
+            result = result * 100
+            result_ = pd.DataFrame(result, index=['正常', '异常', '故障'], columns=['result'])
+            result_dict = {'0': round(result[0], 4), '1': round(result[1], 4), '2': round(result[2], 4)}
+
+            state = max(result_dict, key=lambda x: result_dict[x])
+
+        broken_result = ','.join(broken_result)  # 取出列表中的所有元素
+        broken_reason = ','.join(broken_reason)
+
+
+        '''模型三结果存入数据库'''
+        Ins2Orc.insert_(state=state, b_r=broken_result, result_dict=result_dict, DEVICECODE=DEVICECODE,
+                        result_cp=result_cp,
+                        end_result_cp=end_result_cp, broken_reason=broken_reason)
+        # time.sleep(5)
 
 
 if __name__ == '__main__':
-    asdf,clock = config()
-    print(clock)
-    print(type(clock))
-    schedule.every().day.at(clock).do(main)   #如果需要修改时间点的话可以在这里修改。
+    num_day_, clock = config()
+    num_day_ = num_day_.replace(' ', '')
+    num_day = []
+    [num_day.append(int(i)) for i in re.split('[,:]', num_day_)]
+    if operation_cycles == 0:
+        main()
+    elif operation_cycles == 1:
+        num_day = num_day[:1]
+        if (datetime.datetime.now().day in num_day) and (str(datetime.datetime.now().time())[:5] == clock):
+            main()
+    elif operation_cycles == 2:
+        if (datetime.datetime.now().day in num_day) and (str(datetime.datetime.now().time())[:5] == clock):
+            main()
+    elif operation_cycles == 3:
+        if str(datetime.datetime.now().time())[:5] == clock:
+            main()
+    else:
+        pass
+    # schedule.every().day.at(clock).do(main)   #如果需要修改时间点的话可以在这里修改。
     while True:
         schedule.run_pending()
-
