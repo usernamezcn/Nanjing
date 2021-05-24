eb82021f74ef6728f82482ad3df01b15c0b55403
子辛 <48110583+usernamezcn@users.noreply.github.com>
2021-05-24 17:11:17 +0800

    Attation!!!这个需要更改数据库，配合更改过的数据库，方可以使用

--- parameter_Definition.py
+++ parameter_Definition.py
@@ -8,7 +8,10 @@ import copy
 
 '''从数据库中获取数据'''
 # connect参数  用户名、密码、host地址：端口、服务名
-test_oracle=TestOracle('mw_app','app','192.168.2.200', '1521', 'ORCL')
+# test_oracle=TestOracle('mw_app','app','192.168.2.200', '1521', 'ORCL')
+test_oracle=TestOracle()
+# test_oracle=TestOracle('mw_app','app','127.0.0.1', '1521', 'DBORCALE')
+
 print("连接成功")
 def str_to_int(num):
     a,b = num.split('/')
@@ -30,10 +33,14 @@ def str_to_list(S_list):
 sql_select = 'select PARAM,PARAM_VALUE from bht_sys_config'
 data_oracle= pd.DataFrame(test_oracle.select(sql_select))
 data_oracle.set_index(['PARAM'],inplace=True)
-num_x1_miss,num_x2_miss,num_repeat,jump,num_zero,sliding = \
-    int(data_oracle.loc['num_x1_miss'].values[0]),int(data_oracle.loc['num_x2_miss'].values[0]), \
-    int(data_oracle.loc['num_repeat'].values[0]),int(data_oracle.loc['jump'].values[0]), \
-    int(data_oracle.loc['num_zero'].values[0]),int(data_oracle.loc['sliding'].values[0])
+num_x1_miss,num_x2_miss,num_repeat,jump,num_zero,sliding,operation_cycles = \
+    str_to_list(data_oracle.loc['num_x1_miss'].values[0]),str_to_list(data_oracle.loc['num_x2_miss'].values[0]), \
+    str_to_list(data_oracle.loc['num_repeat'].values[0]),str_to_list(data_oracle.loc['jump'].values[0]), \
+    str_to_list(data_oracle.loc['num_zero'].values[0]),str_to_list(data_oracle.loc['sliding'].values[0]), \
+    int(data_oracle.loc['operation_cycles'])
+threshold, point_1, slide, point,thresholdSudden, point_1Sudden, slideSudden, pointSudden = float(data_oracle.loc['threshold'].values[0]), \
+    int(data_oracle.loc['point_1'].values[0]),int(data_oracle.loc['slide'].values[0]),int(data_oracle.loc['point'].values[0]),float(data_oracle.loc['thresholdSudden'].values[0]), \
+    int(data_oracle.loc['point_1Sudden'].values[0]), int(data_oracle.loc['slideSudden'].values[0]), int(data_oracle.loc['pointSudden'].values[0])
 S_mat_origin = str_to_list(data_oracle.loc['S_mat_origin'].values[0])
 s3_mat_origin = str_to_list(data_oracle.loc['s3_mat_origin'].values[0])
 s7_mat_origin,s8_mat_origin,s9_mat_origin,s10_mat_origin,s11_mat_origin, \
@@ -50,4 +57,3 @@ str_to_list(data_oracle.loc['s16_mat_origin'].values[0]),str_to_list(data_oracle
 # DEVICECODE = data_oracle['DEVICECODE'][0]
 # print('DEVICECODE ：',DEVICECODE)
 # print(data_oracle)
-
