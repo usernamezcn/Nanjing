a80cb56172585875b77e8a400765ebddf6717d2b
子辛 <48110583+usernamezcn@users.noreply.github.com>
2021-05-24 16:49:25 +0800

    重新定义需要评估的监测类型及评估指标
    重新定义需要评估的监测类型及评估指标

--- fault_/fault_describe.py
+++ fault_/fault_describe.py
@@ -19,3 +19,32 @@ univariate  = {'油中溶解气体':'H2','SF6气体水分':'MOISTURE','SF6气体
                '套管压力监测':'none','变压器局部放电':'none','顶层油温（天山换流站）':'OILTEMPERATURE','视频监测':'none',
                '图像监测':'none','微水（天山换流站）':'MOISTURE','避雷器绝缘监测（天山换流站）':'TOTALCURRENT',
                '铁芯接地电流（天山换流站）':'TOTALCORECURRENT','油中溶解气体（天山换流站）':'H2','杆塔倾斜监测':'none'}
+
+newUnivariate  = {'油中溶解气体':'H2','SF6气体水分':'MOISTURE','SF6气体压力':'PRESSURE20C','避雷器绝缘监测':'TOTALCURRENT',
+               '断路器局部放电':'DISCHARGECAPACITY','储能电机工作状态':'none','顶层油温':'OILTEMPERATURE',
+               '分合闸线圈电流波形':'none','容性设备绝缘监测':'CAPACITANCE','铁芯接地电流':'TotalCoreCurrent',
+               '微水':'MOISTURE','覆冰监测':'EQUAL_ICETHICKNESS','导线温度监测':'Line_Temperature1','导线舞动':'none',
+               '风偏监测':'none','微风振动监测':'none','微气象监测':'none',
+               '现场污秽度监测':'ESDD','倒置式电流互感器压力监测':'none','SF6气体压力（天山换流站）':'PRESSURE20C',
+               '套管压力监测':'none','变压器局部放电':'none','顶层油温（天山换流站）':'OILTEMPERATURE','视频监测':'none',
+               '图像监测':'none','微水（天山换流站）':'MOISTURE','避雷器绝缘监测（天山换流站）':'TOTALCURRENT',
+               '铁芯接地电流（天山换流站）':'TOTALCORECURRENT','油中溶解气体（天山换流站）':'H2','杆塔倾斜监测':'Inclination'}
+
+'''
+{'油中溶解气体':'H2','SF6气体水分':'MOISTURE','SF6气体压力':'PRESSURE20C','避雷器绝缘监测':'TOTALCURRENT',
+'断路器局部放电':'DISCHARGECAPACITY','储能电机工作状态':'CHARGETIME','顶层油温':'OILTEMPERATURE',
+'分合闸线圈电流波形':'ACTION','容性设备绝缘监测':'CAPACITANCE','铁芯接地电流':'TOTALCORECURRENT',
+'微水':'MOISTURE','覆冰监测':'EQUAL_ICETHICKNESS','导线温度监测':'Line_Temperature1','导线舞动':'U_VERTICAL_AMPLITUDE',
+'风偏监测':'Windage_Yaw_Angle','微风振动监测':'Vibration_Amplitude','微气象监测':'Average_WindSpeed',
+'现场污秽度监测':'ESDD','SF6气体压力（天山换流站）':'PRESSURE20C','顶层油温（天山换流站）':'OILTEMPERATURE',
+'微水（天山换流站）':'MOISTURE','避雷器绝缘监测（天山换流站）':'TOTALCURRENT',
+'铁芯接地电流（天山换流站）':'TOTALCORECURRENT','油中溶解气体（天山换流站）':'H2'}
+'''
+
+
+univariate_dist = {'H2':1,'MOISTURE':2,'PRESSURE20C':3,'TOTALCURRENT':4,'DISCHARGECAPACITY':4,'CHARGETIME':5,
+                   'OILTEMPERATURE':6,'ACTION':7,'CAPACITANCE':8,'TOTALCORECURRENT':9,
+                   'MOISTURE':10,'EQUAL_ICETHICKNESS':11,'Line_Temperature1':12,'U_VERTICAL_AMPLITUDE':13,
+                   'Windage_Yaw_Angle':14,'Vibration_Amplitude':15,'Average_WindSpeed':16,
+                   'ESDD':17,'PRESSURE20C':18,'OILTEMPERATURE':19,'MOISTURE':20,'TOTALCURRENT':21,
+                   'TOTALCORECURRENT':22,'H2':23}
